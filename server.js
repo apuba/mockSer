574e0b2cccbbba4556f9d1271af8981b9185bbd6
king <3603317@qq.com>
2018-11-23 16:14:05 +0800

    添加跨域设置支持

--- server.js
+++ server.js
@@ -13,7 +13,16 @@ let port = 4000 ; // 监听的端口
  * @param  {[type]} req  [客户端发过来的请求所带数据]
  * @param  {[type]} res  [服务端的相应对象，可使用res.send返回数据，res.json返回json数据，res.down返回下载文件]
  */
-
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+  });
+  
 routers.map(item => {
     let url = item.url.indexOf('/') === 0 ? item.url : '/' + item.url;
     app.all(url, function (req, res) {
@@ -27,6 +36,7 @@ routers.map(item => {
         }
     });
 })
+
 /**
  * 监听4000 端口
  */
